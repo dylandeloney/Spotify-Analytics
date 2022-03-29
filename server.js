de29648974a24a7ded476923522b7dbff6d1bba4
Dylan Deloney <dylandeloney@live.com>
2022-03-29 16:35:18 -0500

    made changes for deployment

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const authRouter = require("./routes/auth");
+const path = require("path");
 
 const app = express();
 app.use(express.json());
@@ -7,5 +8,14 @@ app.use(express.json());
 //Use Routes
 app.use("/", authRouter);
 
-const port = 5000;
+//Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static("client/build"));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+	});
+}
+
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server started on port ${port}`));
